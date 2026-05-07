d108cb83d85ed50bab4b9b63fec1565161f597b3
Gerrit-Jan Hilderink | Nubix B.V. <g.j.hilderink@nubix.nl>
2026-05-07 15:59:14 +0200

    feat: add soccer ball SVG favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'BABB Portaal')</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <script>
         tailwind.config = {
             theme: {
